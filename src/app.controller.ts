49da4833678cd9ec347da186f7a0140fc1c1b46a
ninejuan <ninejuan@protonmail.com>
2024-06-13 01:29:07 +0900

    docs: Hello World!

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,3 +1,4 @@
+import { ApiTags, ApiOperation, ApiResponse, ApiProperty, ApiParam, ApiExcludeEndpoint } from '@nestjs/swagger';
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
@@ -5,6 +6,10 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @ApiOperation({
+		summary: "Hello World!",
+		description: "세상에게 인사합니다."
+	})
   @Get()
   getHello(): string {
     return this.appService.getHello();
